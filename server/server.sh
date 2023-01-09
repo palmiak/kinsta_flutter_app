9630b7da699efd5ec0154f99ea045ab956e35af4
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-01-09 12:57:24 +0100

    minor change to server

--- server/server.sh
+++ server/server.sh
@@ -3,8 +3,8 @@ sh
 #!/bin/bash
 
 # Stop any program currently running on the set port
-echo 'preparing port' $PORT '...'
-fuser -k 5000/tcp
+#echo 'preparing port' $PORT '...'
+#fuser -k 5000/tcp
 
 # switch directories
 cd build/web/
